0f17b5cc3a37c11296116223956bf4640786a210
Puja Sahu <78893474+psahu-deqode@users.noreply.github.com>
2021-07-19 20:12:21 +0530

    Delete src directory

--- src/component/Day3Components/InputRange.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const InputRange = (props) => {
-    return <div> <label >Points (between 0 and 100):</label>
-        <input type="range" id="points" name="points" min="0" max="100" /><br /></div>;
-}
-
-export default InputRange;
